66ce6ced4a2cf1158cfac04b8d0e895a743e3304
Evelyn Hernandez <hernandeze791@gmail.com>
2021-06-04 21:04:11 -0500

    Header component and styling created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from './components/Header/Header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
 
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <span className="header">🎬 Entertainment Hub 🎥</span>
+ 
+    )
+}
+
+export default Header
+
