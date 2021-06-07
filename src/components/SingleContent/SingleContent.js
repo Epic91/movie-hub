6cc1e81403255465bb28ee0a7d7eef0451879472
Evelyn Hernandez <hernandeze791@gmail.com>
2021-06-07 18:14:47 -0500

    trending api info displayed, styling has been added

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { img_300, unavailable } from '../../config/config'
+import './SingleContent.css'
 
 const SingleContent = ({
     id,
@@ -11,8 +12,17 @@ const SingleContent = ({
     vote
 }) => {
     return (
-        <div>
-            <img src=  {poster ? `${img_300}/${poster}` : {unavailable}} />
+        <div className="media">
+            <img 
+                className="poster"
+                src=  {poster ? `${img_300}/${poster}` : {unavailable}} 
+                alt={title}
+                />
+            <b className="title">{title}</b>
+            <span className='subtitle'>
+                {type === 'tv' ? 'TV Series' : 'Movie'}
+                <span className='subtitle'>{release}</span>
+            </span>
         </div>
     )
 }
