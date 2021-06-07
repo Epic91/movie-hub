268e1fa8a259391081fd7605edc8ba1ed74b2ed0
Evelyn Hernandez <hernandeze791@gmail.com>
2021-06-07 17:11:21 -0500

    added env file and fetched API

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,4 +1,11 @@
+import axios from "axios"
+
 const Trending = () => {
+
+    const fetchTrending = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`);
+    }
+
     return (
         <div>
             <span className="pageTitle">Trending </span>
