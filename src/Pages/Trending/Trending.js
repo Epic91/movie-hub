51ee2a177d7c8d384ddac6902e64f3516372c196
Evelyn Hernandez <hernandeze791@gmail.com>
2021-06-07 17:28:23 -0500

    set the state and used use effect hook

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,10 +1,18 @@
-import axios from "axios"
+import axios from "axios";
+import { useEffect, useState} from 'react';
 
 const Trending = () => {
 
+    const [content, setContent] = useState([]);
+
     const fetchTrending = async () => {
         const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`);
-    }
+        setContent(data.results);
+    };
+
+    useEffect(() => {
+        fetchTrending();
+    }, [])
 
     return (
         <div>
