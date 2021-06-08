12d6655759bbfe7aa3b8409a8abcada4e026ac35
Evelyn Hernandez <hernandeze791@gmail.com>
2021-06-08 15:05:33 -0500

    added CSS file for Trending

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState} from 'react';
 import SingleContent from "../../components/SingleContent/SingleContent";
+import './Trending.css'
 
 const Trending = () => {
 
