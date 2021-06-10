25d3b4131401b9e9da5264bc8001cfd679d8a233
Evelyn Hernandez <hernandeze791@gmail.com>
2021-06-10 08:59:04 -0500

    finished genres

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -2,6 +2,10 @@ import { Chip } from "@material-ui/core";
 import axios from "axios"
 import { useEffect } from 'react';
 
+//fetch the genres
+//add a genre
+//delete a genre
+
 const Genres = ({
     selectedGenres,
     setSelectedGenres,
@@ -11,31 +15,57 @@ const Genres = ({
     setPage,
 }) => {
 
+    const handleAdd = (genre) => {
+        //add everything inside of selected genres, and add the genre that is being sent
+        setSelectedGenres([...selectedGenres, genre]);
+        //remove the genre that is being sent in
+        setGenres(genres.filter((g) => g.id !== genre.id));
+        setPage(1);
+    }
+
+    const handleRemove = (genre) => {
+        setSelectedGenres(
+            selectedGenres.filter((selected) => selected.id !== genre.id)
+        );
+        setGenres([...genres, genre]);
+        setPage(1);
+    }
+
     const fetchGenres = async () => {
        const { data } = await axios.get(`https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
         setGenres(data.genres)
     };
 
-    console.log(genres)
-
     useEffect(() => {
         fetchGenres();
         return () => {
             setGenres({});
-        }
+        }; // eslint-disable-next-line
     }, [])
 
 
     return (
         <div
             style={{ padding: "6px 0" }}>
-            {genres && genres.map((genre) => (
+                {selectedGenres.map((genre) => (
                 <Chip 
                     key={genre.id}
                     label={genre.name}
                     style={{margin: 2}}   
                     clickable 
                     size='small'
+                    color="primary"
+                    onDelete={() => handleRemove(genre)}
+                />
+                ))}
+            {genres.map((genre) => (
+                <Chip 
+                    key={genre.id}
+                    label={genre.name}
+                    style={{margin: 2}}   
+                    clickable 
+                    size='small'
+                    onClick={() => handleAdd(genre)}
                 />
             ))}
         </div>
